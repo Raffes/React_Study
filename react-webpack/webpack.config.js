d435e7174450dbd7f211ac6ff6590e2c5567ae61
Rafaela Peres <rafaelaperes215@gmail.com>
2021-02-09 22:50:48 -0300

    Atualizado

--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -4,9 +4,20 @@ const path = require('path')
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index'),
+
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/dist/'
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js$/, //vai compilar tudo que tem a extensão js
+            exclude: /node_modules/, // não vai ser lido / ignorado
+            include: /src/, //onde vai ser lido os arquivos
+            loader: 'babel-loader' // o que vai fazer o loader 
+
+        }]
     }
 }
