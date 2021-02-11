d43ef284c9c053bb5731e7db4a8453d464988e4f
Rafaela Peres <rafaelaperes215@gmail.com>
2021-02-11 16:57:57 -0300

    Atualizado

---
+++ react-webpack/server.js
@@ -0,0 +1,18 @@
+'user strict'
+
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require("./webpack.config")
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    starts: {color: true}
+}).listen(3000, (err) => {
+    if(err) {
+        return console.log(err)
+    }
+
+    console.log('Listening on http://localhost: 3000')
+})
